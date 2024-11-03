6497bd2e41c0db0b531ff219a253d889af5f09e1
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2024-11-03 07:13:36 +0000

    Update 01-backend.tf

--- 01-backend.tf
+++ 01-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "project.car-state-bucket"
+    bucket         = "project.car"
     key            = "project.car/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "project.car-state-dynamo"
