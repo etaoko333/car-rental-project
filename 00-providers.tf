e0c441e79436035a9814a79bd41043c9ef583c72
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2024-11-03 07:12:58 +0000

    Update 00-providers.tf

--- 00-providers.tf
+++ 00-providers.tf
@@ -1,6 +1,6 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
-  region  = us-west-1
+  region  = "us-west-1"
 
   default_tags {
     tags = {
