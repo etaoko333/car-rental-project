7ef5d9ab739238312a5c7a3e50180232d1c9ab8a
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2024-11-14 09:39:26 +0000

    Update creation_s3_ dynamodb_state.tf

--- remote_backend/creation_s3_ dynamodb_state.tf
+++ remote_backend/creation_s3_ dynamodb_state.tf
@@ -2,19 +2,8 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "project.car"
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "project.car-state-dynamo"
+  name           = "project.car"
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20
