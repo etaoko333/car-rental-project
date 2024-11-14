f8e0846242938161001f30bed1c3cd12a576c76f
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2024-11-14 09:14:08 +0000

    Update 04-security_groups.tf

--- 04-security_groups.tf
+++ 04-security_groups.tf
@@ -1,30 +1,34 @@
-# Security group for Application Load Balancer
+# create security group for the application load balancer
 resource "aws_security_group" "alb_security_group" {
   name        = "${var.project_name}-${var.environment}-alb-sg"
-  description = "Enable HTTP/HTTPS access on port 80/443"
+  description = "enable http/https access on port 80/443"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description = "HTTP access"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "HTTPS access"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "https access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "${var.project_name}-${var.environment}-alb-sg"
   }
 }
 
@@ -53,62 +57,71 @@ resource "aws_security_group" "bastion_security_group" {
   }
 }
 
-# Security group for Application Server
+
+# create security group for the app server
 resource "aws_security_group" "app_server_security_group" {
   name        = "${var.project_name}-${var.environment}-app-server-sg"
-  description = "Enable HTTP/HTTPS access on port 80/443 via ALB SG"
+  description = "enable http/https access on port 80/443 via alb sg"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description     = "HTTP access from ALB"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_security_group.id]
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb_security_group.id]
   }
 
   ingress {
-    description     = "HTTPS access from ALB"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_security_group.id]
+    description      = "https access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb_security_group.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  // Allow all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "${var.project_name}-${var.environment}-app-server-sg"
   }
 }
 
-# Security group for Database
+# create security group for the database
 resource "aws_security_group" "database_security_group" {
   name        = "${var.project_name}-${var.environment}-database-sg"
-  description = "Enable MySQL/Aurora access on port 3306"
+  description = "enable mysql/aurora access on port 3306"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description     = "MySQL/Aurora access from App Server"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app_server_security_group.id]
+    description      = "mysql/aurora access"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.app_server_security_group.id]
   }
 
   ingress {
-    description     = "MySQL/Aurora access from Bastion Host"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_security_group.id]
+    description      = "custom access"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.bastion_security_group.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  // Allow all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "${var.project_name}-${var.environment}-database-sg"
   }
 }
